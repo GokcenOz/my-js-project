e92e6b47594d7c6df8d162b2ff2e53915def8feb
Gokcen <gokcenucarozsoy@gmail.com>
2021-09-02 17:56:32 +0300

    commit all

---
+++ Day01_HelloWorld.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+console.log(10+4);
+// Create a class : Day01_HelloWorld.js
+// How to run js code on IDE?
+// We need Node JS to run javascript outside of the browser
+// node CLASSNAME
+// E.G. node Day01_HelloWorld.js
